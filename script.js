4df85d329eb62369847dd449772fc570ee11a1f1
jourm <jourm91@gmail.com>
2020-02-14 15:19:35 +0000

    lesson complete

--- script.js
+++ script.js
@@ -19,6 +19,27 @@ $(document).ready(function() {
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream3").addClass('highlight_stream');
 	});
-
-
+    $("p").on("click", function(){
+        $("p").addClass("red-text");
+    });
+    $("h2").mouseenter(function(){
+        $("h2").addClass("blue-background");
+    });
+     $("h2").mouseleave(function(){
+        $("h2").removeClass("blue-background");
+    });
+    $("#button_effects1").click(function(){
+      /*   $("#button_effects1").hide(1000); */
+        $("#van").toggle();
+    });
+    $("#button_effects1").mouseenter(function(){
+        $("#button_effect1").fadeTo(1000, 0.5);
+    });
+     $("#button_effects1").mouseleave(function(){
+        $("#button_effect1").fadeTo(100, 1);
+    });
+    $("#button_effects1").mouseenter(function(){
+        $('#button_effects1').fadeTo(1000, 0.5);
+    });
 }); 
+
